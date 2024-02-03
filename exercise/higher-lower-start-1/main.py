d8490d5a707f2ab9255515fb6edd12921bf2a714
RameshPlakkal <ramesh.plakkal@gmail.com>
2024-02-03 15:41:58 +0530

    HigherLower Game
    Final version of the higher-lower game

--- exercise/higher-lower-start-1/main.py
+++ exercise/higher-lower-start-1/main.py
@@ -6,6 +6,7 @@ from replit import clear
 GAME_ON = True
 SCORE = 0
 
+
 def generate_random_selector():
   return random.choice(data)
 
@@ -13,7 +14,7 @@ def generate_random_selector():
 def win_or_loss(selector1, selector2):
   """Takes inputs as data to compare
      Return results of the comparison   
-  """    
+  """
   if selector1["follower_count"] > selector2["follower_count"]:
     return "A"
   else:
@@ -22,64 +23,36 @@ def win_or_loss(selector1, selector2):
 
 def game_on(selector_1, selector_2):
   """ Starts the game and continues till user enters incorrect answer
-  """  
+  """
 
   global SCORE
-  clear()
   print(logo)
-  
+
   print(f"Compare A: {selector_1['name']}, {selector_1['description']}, from {selector_1['country']}")
 
   print(vs)
 
   print(f"Against B: {selector_2['name']}, {selector_2['description']}, from {selector_2['country']}")
 
-  # print(selector_1["follower_count"],"\n",selector_2["follower_count"])
-  
   user_input = input("Who has more followers? Type 'A' or 'B' : ")
   result = win_or_loss(selector_1, selector_2)
-
+  clear()
+  
   if result == user_input:
     SCORE += 1
     print(f"You are right! Current score is : {SCORE}")
     if result == "A":
       game_on(selector_1, generate_random_selector())
     else:
-      game_on(selector_2, generate_random_selector())    
+      game_on(selector_2, generate_random_selector())
   else:
     print(f"Sorry, that's wrong. Final score is : {SCORE}")
-
-
 
 
 selector_1 = generate_random_selector()
 selector_2 = generate_random_selector()
 
-
 if selector_1 == selector_2:
   selector_2 = generate_random_selector()
 
-
 game_on(selector_1, selector_2)
-
-# selector1 = {
-#   'name': 'Cristiano Ronaldo',
-#   'follower_count': 215,
-#   'description': 'Footballer',
-#   'country': 'Portugal'
-# }
-
-# selector2 = {
-#   'name': 'Cristiano Ronaldo',
-#   'follower_count': 215,
-#   'description': 'Footballer',
-#   'country': 'Portugal'
-# }
-
-# if selector1 == selector2:
-#   selector2 = generate_random_selector()
-#   print(selector2)
-# else:
-#   print("not")
-
-
